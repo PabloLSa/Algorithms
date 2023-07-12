9008f80f7fc55a41d1c726c9510f44c3887816b0
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-07-12 15:55:19 -0300

    :sparkles: Algoritmo de busca
    Número de estudantes estudando no mesmo horário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,21 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    count = 0
+
+    for period in permanence_period:
+        entry_time, exit_time = period
+
+        if type(entry_time) != int or type(exit_time) != int:
+            return None
+
+        if entry_time <= target_time and exit_time >= target_time:
+            count += 1
+
+    return count
+
+    
+
+# permanence_period = [{12,16}, {10,14}, {11,15}, {9,13}]

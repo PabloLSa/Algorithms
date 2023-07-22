a849860588e63d2f1d6c7b1fa8fc71ebe0c58c03
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-07-22 18:23:13 -0300

    :ambulance:5
     correcao de lint

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,12 +1,10 @@
 def find_duplicate(nums):
     """
-    Encontra o primeiro número duplicado em uma lista de números inteiros positivos.
-
-    Args:
+    
         nums (list): Uma lista de números inteiros positivos.
 
     Returns:
-        int or False: O primeiro número duplicado encontrado ou False se não houver duplicatas.
+      
     """
     # Verificar se a entrada é válida
     if not all(isinstance(num, int) and num > 0 for num in nums):

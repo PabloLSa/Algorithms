2cd3e6c1dff1e8f302cde61b5a6ce15c10b4e5e8
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-07-22 17:37:35 -0300

    :sparkles:3
    3 - Palíndromos (Recursividade)

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,22 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    Verifica se a palavra é um palíndromo de forma recursiva.
+
+    """
+    # Caso base: se a palavra for vazia, não é um palíndromo
+    if len(word) == 0:
+        return False
+
+    # Caso base: se o índice inicial for maior ou igual ao índice final, a palavra é um palíndromo
+    if low_index >= high_index:
+        return True
+
+    # Verifica se os caracteres nos índices baixo e alto são iguais
+    first = word[low_index]
+    last = word[high_index]
+    if first == last:
+        # Recursivamente, verifica o restante da palavra, movendo os índices para dentro
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
+

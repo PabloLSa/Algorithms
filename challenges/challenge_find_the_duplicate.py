758db66cce7ca1ec633a143c8a091d2343b3547b
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-07-22 18:20:33 -0300

    :sparkles:5
     5 - Encontrando números repetidos (Algoritmo de busca)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,23 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    Encontra o primeiro número duplicado em uma lista de números inteiros positivos.
+
+    Args:
+        nums (list): Uma lista de números inteiros positivos.
+
+    Returns:
+        int or False: O primeiro número duplicado encontrado ou False se não houver duplicatas.
+    """
+    # Verificar se a entrada é válida
+    if not all(isinstance(num, int) and num > 0 for num in nums):
+        return False
+
+    num_set = set()
+
+    for num in nums:
+        if num in num_set:
+            return num
+
+        num_set.add(num)
+
+    return False

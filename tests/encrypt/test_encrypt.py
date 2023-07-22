cefc32f3c674b12c8295003e15e2e0043d298ccc
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-07-22 16:54:13 -0300

    :sparkles:2
    2 - Criptografia de inversões (Testes)

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,26 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
     pass
+    assert encrypt_message("Olá Mundo", 1) == (
+        "O_odnuM ál"
+    )
+    assert encrypt_message("Olá Mundo", 7) == (
+        "nuM álO_od"
+    )
+    assert encrypt_message("Olá Mundo", 12) == (
+        "odnuM álO"
+    )
+    assert encrypt_message("Olá Mundo", 15) == (
+        "odnuM álO"
+    )
+    assert encrypt_message("Olá Mundo", -150) == (
+        "odnuM álO"
+    )
+
+    with pytest.raises(TypeError):
+        encrypt_message("Olá Mundo", "8")
+        encrypt_message(1892981273, 6)
+

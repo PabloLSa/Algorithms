08f00b14f911ef1b0dba5f9a6a95f730128c845c
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-07-22 17:45:19 -0300

    :ambulance:3
    3 - Correçao de lint

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -7,16 +7,12 @@ def is_palindrome_recursive(word, low_index, high_index):
     if len(word) == 0:
         return False
 
-    # Caso base: se o índice inicial for maior ou igual ao índice final, a palavra é um palíndromo
     if low_index >= high_index:
         return True
-
     # Verifica se os caracteres nos índices baixo e alto são iguais
     first = word[low_index]
     last = word[high_index]
     if first == last:
-        # Recursivamente, verifica o restante da palavra, movendo os índices para dentro
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     else:
         return False
-
